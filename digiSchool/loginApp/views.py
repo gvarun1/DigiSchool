c33ec6be9f36e11d19c46fd7a017f489d0fff8aa
Sumit Nagle <76628590+Asdvamp@users.noreply.github.com>
2022-03-16 01:29:45 +0530

    loginApp - contact page added, view functions modified

--- digiSchool/loginApp/views.py
+++ digiSchool/loginApp/views.py
@@ -3,45 +3,47 @@ from django.http import HttpRequest, HttpResponse
 from django.template import Template, Context
 from digiSchool.loginApp import models as login_model
 from django.middleware import csrf
-# Create your views here.
 
+
+#-----------------------------------Completed View functions-------------------------------------
 def signUpPage(request):
 	csrf_token = csrf.get_token(request)
-	templateto = Template(r'''<form action="/signup/status/" method="POST">{% csrf_token %}<input type="text" name="fn"><input type="text" name="ln"><input type="text" name="ea"><input type="text" name="cd"><input type="text" name="cl"><input type="text" name="cs"><input type="text" name="sn"><input type="text" name="pswd"><input type="submit" value="Create"></form>''')
-	#return HttpResponse(templateto.render(Context({"csrf_token":csrf_token})))
-	return render(request, 'signup_page.html', {"csrf_token":csrf_token})
+	return render(request, 'signup_page.html', {"csrf_token":csrf_token , "error_signing" : False, "user_exist": False})
 
 
 def signUpPosted(request):
-	# A security check
+	# Security check for method-interchange vaulnerablity: https://blog.nvisium.com/method-interchange-forgotten
 	if request.GET or len(request.GET) > 0:
-		"""A Malicious user trying to change the method, to look for sec breach"""
 		return HttpResponse('<body><meta http-equiv="refresh" content="0; url="http://127.0.0.1:8000/signup/"/></body>')
-	
-	uInput = request.POST
-	f_name, l_name = uInput.get("fn", False), uInput.get("ln", False)
-	e_addr, contact = uInput.get("ea", False), uInput.get("cd", False)
-	cls_int, cls_sec = uInput.get("cl", False), uInput.get("cs", False)
-	s_name = uInput.get("sn", False)
-	passwd = uInput.get("pswd", False)
-	rollnumber = uInput.get("rn", False)
-	
-	"""****************Passwd is passed through our hashing algorithm***********************************"""
 
+	csrf_token = csrf.get_token(request)
+	
+	requestInput = request.POST
+	f_name, l_name = requestInput.get("fn", False), requestInput.get("ln", False)
+	e_addr, contact = requestInput.get("ea", False), requestInput.get("cd", False)
+	cls_int, cls_sec = requestInput.get("cl", False), requestInput.get("cs", False)
+	rollnumber, s_name = requestInput.get("rn", False), requestInput.get("sn", False)
+	passwd = requestInput.get("pswd", False)
+	
+	"""****************passwd is passed through our hashing algorithm***********************************"""
 	# After this, the "passwd" contains a Hashed + Salted password.
 
 	if len(login_model.UserDB.objects.filter(email_addr=e_addr)) > 0:
-		return HttpResponse("<p>user already exists.</p>") # Create a redirect to login page.
+		return render(request, 'signup_page.html', {"csrf_token":csrf_token , "error_signing" : False, "user_exist": True})
+	
 	try:
-		setting_user = login_model.UserDB(first_name = f_name, last_name = l_name, email_addr=e_addr, class_int=cls_int, class_section=cls_sec, school_name = s_name, contact_detail=contact, passwd=passwd, rollnumber=rollnumber)
+		setting_user = login_model.UserDB(first_name = f_name, last_name = l_name, email_addr=e_addr, class_int=cls_int, class_section=cls_sec, rollnumber=rollnumber, school_name = s_name, contact_detail=contact, passwd=passwd)
 	except:
-		# Replace with the actual error page. and finallly return to the sign up page.
-		return HttpResponse('<p> Error in some data, please signup again.</p><meta http-equiv="refresh" content="1; url="http://127.0.0.1:8000/signup/"/>')
+		return render(request, 'signup_page.html', {"csrf_token":csrf_token , "error_signing" : True, "user_exist": False})
 	setting_user.save()
 
-	return HttpResponse(f"User {f_name} Succesfully created.")
+	return render(request, 'signup_success.html')
 
+#-----------------------------------In-Progress View functions.-------------------------------------
 
+def contactPage(request):
+	csrf_token = csrf.get_token(request)
+	return render(request, 'contact_page.html', {"csrf_token":csrf_token})
 
 def loginPage(request):
 	if len(request.POST) == 3 and request.POST.get("uname", False) and request.POST.get("pswd", False):
@@ -50,9 +52,9 @@ def loginPage(request):
 			"""A Malicious user trying to change the method, to look for sec breach"""
 			return HttpResponse('<body><meta http-equiv="refresh" content="0; url="http://127.0.0.1:8000/login/"/></body>')
 		
-		uInput = request.POST
-		uname = uInput.get("uname", False)
-		passwd = uInput.get("pswd", False)
+		requestInput = request.POST
+		uname = requestInput.get("uname", False)
+		passwd = requestInput.get("pswd", False)
 
 		if not uname or not passwd:
 			# Password or username is not given.
@@ -77,5 +79,3 @@ def loginPage(request):
 		csrf_token = csrf.get_token(request)
 		templateto = Template(r'''<form action="/login/" method="POST">{% csrf_token %}<input type="text" name="uname"><input type="text" name="pswd"><input type="submit" value="Login"></form>''')
 		return HttpResponse(templateto.render(Context({"csrf_token":csrf_token})))
-
-		
